c8a2c7b202db3f6e7f282daff937956d08741181
EwoutH <EwoutH@users.noreply.github.com>
2021-11-25 09:50:56 +0000

    deploy: 75dfc81b470a81d7777946f10f0d70941ba5772d

--- search/all_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['libpng_5fread_5ft_2',['libpng_read_t',['../structlibpng__read__t.html',1,'']]],
-  ['libpng_5fwrite_5ft_3',['libpng_write_t',['../structlibpng__write__t.html',1,'']]]
-];
